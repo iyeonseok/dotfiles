69cdcb543cb593ca6d56d854e6a9e7cb1022c8f1
Yeonseok <ys-313@naver.com>
2020-06-02 18:49:59 +0900

    starship 설정

--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -1,14 +1,7 @@
-function set_prompt {
-  local reset='\e[0m'
-  local reverse='\e[7m'
-  local git_branch_command="git branch 2> /dev/null | grep '*' | sed 's/* \(.*\)/ \1/'"
-  PS1="$reverse\u@\h: \w\$($git_branch_command)$reset\n\\$ "
-}
-
 export VISUAL=vim
 
 if [[ -e ~/.git_completion.bash ]]; then
   source ~/.git_completion.bash
 fi
 
-set_prompt
+eval "$(starship init bash)"
