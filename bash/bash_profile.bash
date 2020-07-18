66c61ac748dc2567e28185a48c3998f0075400bc
Yeonseok <ys-313@naver.com>
2020-07-18 16:26:18 +0900

    shell zsh로 교체

--- bash/bash_profile.bash
+++
@@ -1,5 +0,0 @@
-if [[ -e ~/.bash_profile_private ]]; then
-  source ~/.bash_profile_private
-fi
-
-source ~/.bashrc
--- bash/bashrc.bash
+++
@@ -1,5 +0,0 @@
-export VISUAL=vim
-
-source ~/.git_completion.bash
-
-eval "$(starship init bash)"
